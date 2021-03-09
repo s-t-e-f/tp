97acbfc502a48005323aa583a29399bd6de121d0
jovanhuang <jhtc98@hotmail.com>
2021-03-09 23:11:36 +0800

    Fix code to pass checkstyleMain.

--- src/main/java/seedu/duke/Duke.java
+++ src/main/java/seedu/duke/Duke.java
@@ -6,28 +6,28 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    public static final String LOGO_STRING = " _____                                 " +
-            "                 ___              _\n" +
-            "/__   \\ _ __  __ _   ___  ___ /\\_/\\ ___   _   _  _ __  / _ \\ _ __  ___  (_)\n" +
-            "  / /\\/| '__|/ _` | / __|/ _ \\\\_ _// _ \\ | | | || '__|/ /_)/| '__|/ _ \\ | |\n" +
-            " / /   | |  | (_| || (__|  __/ / \\| (_) || |_| || |  / ___/ | |  | (_) || |\n" +
-            " \\/    |_|   \\__,_| \\___|\\___| \\_/ \\___/  \\__,_||_|  \\/     |_|   \\___/_/ |\n" +
-            "                                                                      |__/\n";
+    public static final String LOGO_STRING = " _____                                 "
+            + "                 ___              _\n"
+            + "/__   \\ _ __  __ _   ___  ___ /\\_/\\ ___   _   _  _ __  / _ \\ _ __  ___  (_)\n"
+            + "  / /\\/| '__|/ _` | / __|/ _ \\\\_ _// _ \\ | | | || '__|/ /_)/| '__|/ _ \\ | |\n"
+            + " / /   | |  | (_| || (__|  __/ / \\| (_) || |_| || |  / ___/ | |  | (_) || |\n"
+            + " \\/    |_|   \\__,_| \\___|\\___| \\_/ \\___/  \\__,_||_|  \\/     |_|   \\___/_/ |\n"
+            + "                                                                      |__/\n";
     public static final String PROJECT_TEAM_ID = "Team Project of CS2113-W10-3.";
     public static final String APP_NAME_AND_VERSION = "TraceYourProj - v0.1";
     public static final String HOW_TO_GET_HELP = "Type 'help' for a list of command and related usage.";
-    public static final String ALL_COMMANDS_STRING = "-----------------------------------------" +
-            "-------------------------------\n" +
-            "Here are the available commands:\n" +
-            "add: Adds a resource to a project\n" +
-            "\tFormat: add p/PROJECT_NAME url/URL_LINK [d/LINK_DESCRIPTION]\n" +
-            "delete: Deletes a resource from the resource list based on the project.\n" +
-            "\tFormat: delete p/PROJECT_NAME [i/INDEX]\n" +
-            "list-all: Shows a list of all resources used in all projects.\n" +
-            "exit: Exits the program.\n" +
-            "------------------------------------------------------------------------\n";
-    public static final String EXIT_MESSAGE = "Thank you for using TraceYourProj!\n" +
-            "Hope you have a wonderful day.\n";
+    public static final String ALL_COMMANDS_STRING = "-----------------------------------------"
+            + "-------------------------------\n"
+            + "Here are the available commands:\n"
+            + "add: Adds a resource to a project\n"
+            + "\tFormat: add p/PROJECT_NAME url/URL_LINK [d/LINK_DESCRIPTION]\n"
+            + "delete: Deletes a resource from the resource list based on the project.\n"
+            + "\tFormat: delete p/PROJECT_NAME [i/INDEX]\n"
+            + "list-all: Shows a list of all resources used in all projects.\n"
+            + "exit: Exits the program.\n"
+            + "------------------------------------------------------------------------\n";
+    public static final String EXIT_MESSAGE = "Thank you for using TraceYourProj!\n"
+            + "Hope you have a wonderful day.\n";
     public static final String ADD_COMMAND = "add";
     public static final String DELETE_COMMAND = "delete";
     public static final String EXIT_COMMAND = "exit";
