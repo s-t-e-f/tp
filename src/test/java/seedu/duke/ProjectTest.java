f6a133e6dffa7c2c78cd6a8305e8021871d8dd13
jovanhuang <jhtc98@hotmail.com>
2021-03-10 13:21:01 +0800

    Removed redundant imports.

--- src/test/java/seedu/duke/ProjectTest.java
+++ src/test/java/seedu/duke/ProjectTest.java
@@ -2,11 +2,7 @@ package seedu.duke;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 public class ProjectTest {
 
