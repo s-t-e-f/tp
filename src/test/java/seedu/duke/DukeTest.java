93d03fa703ae327031d485e43992db32599464c4
s-t-e-f <65395025+s-t-e-f@users.noreply.github.com>
2021-03-10 15:57:04 +0800

    JUnit test for deleting a valid resource

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -192,4 +192,40 @@ class DukeTest {
         System.setOut(System.out);
     }
 
+    @Test
+    public void testDeleteFromProject() {
+        ByteArrayOutputStream newOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(newOutputStream));
+
+        String inputToCmd = "add p/CS2113 Group Project url/https://ay2021s2-cs2113-w10-3.github.io/tp/ d/Team Project for CS2113\n"
+                + "add p/CS2113 Group Project url/https://nus-cs2113-ay2021s2.github.io/website/admin/tp-expectations.html d/tp Website\n"
+                + "list-all\n"
+                + "delete p/CS2113 Group Project i/1\n"
+                + "exit";
+
+        System.setIn(new ByteArrayInputStream(inputToCmd.getBytes()));
+
+        Duke.main(null);
+
+        String targetString = dukeStandardHeading
+                + "The resource is added into the new project \"CS2113 Group Project\".\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + "The resource is added to the existing project \"CS2113 Group Project\".\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + "Here is the list of all project(s) and it's resource(s)!\n"
+                + "--------------------------------------------------------" + "\n"
+                + "Project 1: CS2113 Group Project\n"
+                + "Resource(s):\n"
+                + "1): https://ay2021s2-cs2113-w10-3.github.io/tp/ (Description: Team Project for CS2113)\n"
+                + "2): https://nus-cs2113-ay2021s2.github.io/website/admin/tp-expectations.html (Description: tp Website)\n"
+                + "--------------------------------------------------------\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + "The resource is deleted from the project \"CS2113 Group Project\".\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + Duke.EXIT_MESSAGE + "\n";
+
+        assertEquals(newOutputStream.toString(), targetString);
+
+        System.setOut(System.out);
+    }
 }
