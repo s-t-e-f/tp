3a680e97cade4ffcb18e86190894e30caaac975d
yyixue <yanyixue99@gmail.com>
2021-03-15 12:26:07 +0800

    Remove redundant import

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -3,7 +3,6 @@ package seedu.duke;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import jdk.jfr.StackTrace;
 import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
