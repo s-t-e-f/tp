f89b6da93f40bbec458aa9e1facbf982d9044459
yyixue <yanyixue99@gmail.com>
2021-03-23 23:22:39 +0800

    Add JUnit Tests and Assertions for Find

--- src/test/java/seedu/duke/DukeTest.java
+++ src/test/java/seedu/duke/DukeTest.java
@@ -260,6 +260,121 @@ class DukeTest {
     }
 
     @Test
+    public void testFindInProject() {
+        ByteArrayOutputStream newOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(newOutputStream));
+
+        String inputToCmd = "add p/CS2113 Group Project url/https://ay2021s2-cs2113-w10-3.github.io/tp/ d/Team Project for CS2113\n"
+                + "add p/CS2113 Group Project url/https://nus-cs2113-ay2021s2.github.io/website/admin/tp-expectations.html d/tp Website\n"
+                + "list-all\n"
+                + "find k/Website p/CS2113 Group Project\n"
+                + "exit";
+
+        System.setIn(new ByteArrayInputStream(inputToCmd.getBytes()));
+
+        Duke.main(null);
+
+        String targetString = dukeStandardHeading
+                + "The resource is added into the new project \"CS2113 Group Project\".\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + "The resource is added to the existing project \"CS2113 Group Project\".\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + "Here is the list of all project(s) and it's resource(s)!\n"
+                + "--------------------------------------------------------" + "\n"
+                + "Project 1: CS2113 Group Project\n"
+                + "Resource(s):\n"
+                + "1): https://ay2021s2-cs2113-w10-3.github.io/tp/ (Description: Team Project for CS2113)\n"
+                + "2): https://nus-cs2113-ay2021s2.github.io/website/admin/tp-expectations.html (Description: tp Website)\n"
+                + "--------------------------------------------------------\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + "--------------------------------------------------------" + "\n"
+                + "Project: CS2113 Group Project\n"
+                + "1): https://nus-cs2113-ay2021s2.github.io/website/admin/tp-expectations.html (Description: tp Website)\n"
+                + "--------------------------------------------------------\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + Duke.EXIT_MESSAGE + "\n";
+
+        assertEquals(newOutputStream.toString(), targetString);
+
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void testFindInAllProjects() {
+        ByteArrayOutputStream newOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(newOutputStream));
+
+        String inputToCmd = "add p/CS2113 Group Project url/https://ay2021s2-cs2113-w10-3.github.io/tp/ d/Team Project for CS2113\n"
+                + "add p/CS2113 Group Project url/https://nus-cs2113-ay2021s2.github.io/website/admin/tp-expectations.html d/tp Website\n"
+                + "add p/Data Science Project url/https://www.kaggle.com/fedesoriano/stroke-prediction-dataset d/Kaggle Stroke Prediction Dataset Website\n"
+                + "find k/Website\n"
+                + "exit";
+
+        System.setIn(new ByteArrayInputStream(inputToCmd.getBytes()));
+
+        Duke.main(null);
+
+        String targetString = dukeStandardHeading
+                + "The resource is added into the new project \"CS2113 Group Project\".\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + "The resource is added to the existing project \"CS2113 Group Project\".\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + "The resource is added into the new project \"Data Science Project\".\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + "Here is the list of all project(s) and its resource(s) matching the keyword!\n"
+                + "--------------------------------------------------------" + "\n"
+                + "Project 1: CS2113 Group Project\n"
+                + "1): https://nus-cs2113-ay2021s2.github.io/website/admin/tp-expectations.html (Description: tp Website)\n"
+                + "--------------------------------------------------------\n"
+                + "Project 2: Data Science Project\n"
+                + "1): https://www.kaggle.com/fedesoriano/stroke-prediction-dataset (Description: Kaggle Stroke Prediction Dataset Website)\n"
+                + "--------------------------------------------------------\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + Duke.EXIT_MESSAGE + "\n";
+
+        assertEquals(newOutputStream.toString(), targetString);
+
+        System.setOut(System.out);
+    }
+
+    @Test
+    public void testFindInNotFoundProject() {
+        ByteArrayOutputStream newOutputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(newOutputStream));
+
+        String inputToCmd = "add p/CS2113 Group Project url/https://ay2021s2-cs2113-w10-3.github.io/tp/ d/Team Project for CS2113\n"
+                + "add p/CS2113 Group Project url/https://nus-cs2113-ay2021s2.github.io/website/admin/tp-expectations.html d/tp Website\n"
+                + "list-all\n"
+                + "find k/Website p/Data Science Project\n"
+                + "exit";
+
+        System.setIn(new ByteArrayInputStream(inputToCmd.getBytes()));
+
+        Duke.main(null);
+
+        String targetString = dukeStandardHeading
+                + "The resource is added into the new project \"CS2113 Group Project\".\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + "The resource is added to the existing project \"CS2113 Group Project\".\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + "Here is the list of all project(s) and it's resource(s)!\n"
+                + "--------------------------------------------------------" + "\n"
+                + "Project 1: CS2113 Group Project\n"
+                + "Resource(s):\n"
+                + "1): https://ay2021s2-cs2113-w10-3.github.io/tp/ (Description: Team Project for CS2113)\n"
+                + "2): https://nus-cs2113-ay2021s2.github.io/website/admin/tp-expectations.html (Description: tp Website)\n"
+                + "--------------------------------------------------------\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + "Project cannot be found! Please enter a valid project name!" + "\n"
+                + Duke.SIGNAL_FOR_USER_TO_INPUT
+                + Duke.EXIT_MESSAGE + "\n";
+
+        assertEquals(newOutputStream.toString(), targetString);
+
+        System.setOut(System.out);
+    }
+
+    @Test
     public void testExit() {
         ByteArrayOutputStream newOutputStream = new ByteArrayOutputStream();
         System.setOut(new PrintStream(newOutputStream));
