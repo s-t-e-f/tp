39c102c8cce4acea2d1b0c1b5e2875bca8d229e6
jovanhuang <jhtc98@hotmail.com>
2021-04-10 16:40:54 +0800

    Fix checkstyleTest bug.

--- src/test/java/seedu/duke/project/ProjectManagerTest.java
+++ src/test/java/seedu/duke/project/ProjectManagerTest.java
@@ -3,7 +3,7 @@ package seedu.duke.project;
 import org.junit.jupiter.api.Test;
 
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProjectManagerTest {
     /**
